42a715f8193106cc29ae5e9a5d38d13b83e4feeb
Alex Craig <alexandercraig@oldrochester.org>
2021-05-03 08:50:58 -0400

    added iteration slider

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
+
 function setup()
 {
     createCanvas(windowWidth, windowHeight);
     background(51);
-    noFill();
-    stroke(255);
 
-    iterationSlider = createSlider(1, 10, 1, 1);
-    iterationSlider.position(width / 2 - 60, (5/6) * height);
-    iterationSlider.style('width', '10000');
- 
-    var size = width / 3;
+    // Create iteration slider:
+    iterationSlider = createSlider(1, 8, 1, 1);
+    iterationSlider.position(windowWidth / 2, (5/6) * windowHeight).center('horizontal');
+    //Create size slider:
+
+
+}
+
+function draw()
+{
+    background(51);
+    textSize(20);
+    textStyle(NORMAL);
+    textAlign(CENTER);
+    fill('white');
+    text('Iterations: ' + iterationSlider.value(), windowWidth / 2, (4.95/6) * windowHeight);
+
     noFill();
-    iterateTriangle(width / 2 - size / 2, height / 2 + size / 3, size);
-    
-    function iterateTriangle(x, y, size)
+    stroke('white');
+    let size = width / 3;
+    iterateTriangle(width / 2 - size / 2, height / 2 + size / 3, size, iterationSlider.value())
+}
+
+function windowResized()
+{
+    resizeCanvas(windowWidth, windowHeight);
+    iterationSlider.remove();
+    setup();
+}
+
+function iterateTriangle(x, y, size, iterations)
+{
+    iterations--;
+    if (iterations >= 0)
     {
-        if (size > 5) 
-        {
-            triangle(x, y, x + size, y, x + size / 2, y - size / 3 * 2.6);
-            
-            //Left Triangle:
-            iterateTriangle(x, y, size / 2);
-    
-            //Right Triangle:
-            iterateTriangle(x + size / 2, y, size / 2);
-    
-            //Top Triangle:
-            iterateTriangle(x + size / 4, y - size / 2.3, size / 2);
-        }
+        console.log(iterations)
+        triangle(x, y, x + size, y, x + size / 2, y - size / 3 * 2.6);
+        
+        //Left Triangle:
+        iterateTriangle(x, y, size / 2, iterations);
+
+        //Right Triangle:
+        iterateTriangle(x + size / 2, y, size / 2, iterations);
+        //Top Triangle:
+        iterateTriangle(x + size / 4, y - size / 2.3, size / 2, iterations);
     }
 }
